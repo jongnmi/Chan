37e1f4ebddcfc41db34d17698480d6b4aa4b03e1
Chan <jongnmi00@gmail.com>
2023-03-05 19:58:28 +0900

    SWEA 2027

---
+++ Github/src/Solution2027.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Arrays;
+
+public class Solution2027 {
+
+	public static void main(String[] args) {
+		String array[] = {"+","+","+","+","+"};
+		for(int i=0; i<=4; i++) {
+			array[i] = "#";
+			System.out.println(Arrays.toString(array));
+		}
+	}
+
+}
