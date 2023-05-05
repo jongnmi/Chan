075fb8ecdd5102cfecded13ed18b8a83e49d5a6a
Chan <104296212+jongnmi@users.noreply.github.com>
2023-05-05 10:11:12 +0900

    Continue

---
+++ Github/src/basic/ContinueTest.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class ContinueTest {
+
+	public static void main(String[] args) {
+		// continue : 반복문내에서 continue를 만나면 continue 다음의 실행문을 건너뛴다.
+		for(int i=1; i<=10; i++) {
+			if(i%2==1) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
